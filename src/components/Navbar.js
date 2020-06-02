659811c627ef3898aec86fce9acc810d2ab1c084
AnithaD <anitha.k.dharaneedharan@gmail.com>
2020-06-02 23:47:00 +0100

    Adds styled components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,42 @@
 import React, { Component } from "react";
 import logo from "../logo.svg";
 import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
+import styled from "styled-components";
 
 export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
           <Link to="/">
-            <img src={logo} alt="logo" className="navbar-brand"/>
+            <img src={logo} alt="logo" className="navbar-brand" />
           </Link>
           <ul className="navbar-nav align-items-center">
             <li className="nav-item ml-5">
-            <Link to="/" className="nav-link" >
+              <Link to="/" className="nav-link">
                 products
               </Link>
             </li>
           </ul>
-          <Link to="/cart" >
-                <button>
-                    <i className="fa fa-cart-plus">my cart</i>
-                </button>
-              </Link>
-        </nav>
+          <Link to="/cart" className="ml-auto">
+            <ButtonContainer>
+              <span className="mr-2">
+                <i className="fa fa-cart-plus">my cart</i>
+              </span>
+            </ButtonContainer>
+          </Link>
+        </NavWrapper>
       </div>
     );
   }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem ;
+    text-transform: capitalize;
+  }
+`;
